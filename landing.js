f2dfd25e86ced80b975d387299d2cd00cb1e04e0
resistbrush <71937778+resistbrush@users.noreply.github.com>
2021-04-30 04:50:55 +0300

    Update landing.js

--- landing.js
+++ landing.js
@@ -2,7 +2,7 @@ var visible = false;
 
 document.addEventListener("DOMContentLoaded", function() {
   var iconItem = document.getElementById("icon_item");
-
+//id="icon_item" (Duolingo Icon on navbar) activates function
   iconItem.addEventListener("click", function() {
     if (document.body.clientWidth < 700) hideShowMenu();
   });
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
       };
     });
   }
-
+//hide menu when clientWidth (resized) < 700
   window.addEventListener("resize", function(event) {
     if (document.body.clientWidth >= 700) {
       if (!visible) {
@@ -46,9 +46,12 @@ function validateMyForm() {
   var nameValue = name.value;
   var msgValue = message.value;
 
-  //https://stackoverflow.com/a/32686261/4673960
+//email
+// https://www.w3resource.com/javascript/form/email-validation.php
+//name is assumed min 4 if hieroglyphs
+//message is assumed min 10 for context
   var emailOK = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailValue);
-  var nameOK = nameValue.length >= 2;
+  var nameOK = nameValue.length >= 4;
   var mssgOK = msgValue.length >= 10;
 
 
@@ -57,7 +60,8 @@ function validateMyForm() {
       document.getElementById('error_name').style.display = "none";
     }
 
-    alert("Success, message sent!")
+    alert("Message sent!")
+//clear contents
     email.value = "";
     name.value = "";
     message.value = "";
@@ -68,17 +72,17 @@ function validateMyForm() {
     var errorMessage = "";
 
     if (!nameOK) {
-      errorMessage += "Please enter your name";
+      errorMessage += "Name value error";
     }
 
     if (!emailOK) {
       if (errorMessage.length > 0) errorMessage += "\n";
-      errorMessage += "Please enter a valid email address";
+      errorMessage += "Email value error";
     }
 
     if (!mssgOK) {
       if (errorMessage.length > 0) errorMessage += "<br>";
-      errorMessage += "Your message should be at least 10 characters long";
+      errorMessage += "Min message length: 10 characters";
     }
 
     errorInfo(errorMessage);
